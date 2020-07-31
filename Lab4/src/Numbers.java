ae7f782f1f14c0f3924e899ef88340980406bdf9
StephanieJBuckingham <68093533+StephanieJBuckingham@users.noreply.github.com>
2020-07-30 21:09:48 -0400

    Update Numbers.java

--- Lab4/src/Numbers.java
+++ Lab4/src/Numbers.java
@@ -16,35 +16,32 @@ public class Numbers {
 
 	// Create method that prints next largest integer in string
 	public static void nextLargest(int[] Numbers, int n) {
-		
-		//Make copy of Numbers Array
-		int[] copy= Arrays.copyOf(Numbers, n);
-		
-		//Sort Numbers in array smallest to largest 
+
+		// Make copy of Numbers Array
+		int[] copy = Arrays.copyOf(Numbers, n);
+
+		// Sort Numbers in array smallest to largest
 		Arrays.sort(copy);
-		
-		
+
 		for (int i = 0; i < n - 1; i++) {
-			
 		}
 
-		
 		// Initialize variables
 		int next, i, j;
 
 		for (i = 0; i < n; i++) {
-			
+
 			// Print Integer.MAX_VALUE if there is not value higher than number
 			next = Integer.MAX_VALUE;
 
+			// Find next largest number in array
 			for (j = 0; j < n - 1; j++) {
 				if (copy[j] == Numbers[i]) {
 					next = copy[j + 1];
 					break;
-				
 				}
 			}
-			//Print out array and next largest number
+			// Print out array and next largest number
 			System.out.println(Numbers[i] + ": " + next);
 		}
 	}
